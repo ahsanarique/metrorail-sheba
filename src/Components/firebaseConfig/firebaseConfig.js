9f1e7af5b95fc78bba469b5cfaa39328e818dd42
ahsanarique <ahsan.arique@gmail.com>
2021-03-19 00:53:51 +0600

    initialization and installations

---
+++ src/Components/firebaseConfig/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBjKaOL7Ot0WBz2E3ZGUx-TUGf6OQmVDq0",
+  authDomain: "metrorail-sheba.firebaseapp.com",
+  projectId: "metrorail-sheba",
+  storageBucket: "metrorail-sheba.appspot.com",
+  messagingSenderId: "722426414371",
+  appId: "1:722426414371:web:1def06271f64ba6c007896",
+};
